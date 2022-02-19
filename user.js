2f77286a302aee2b3b0d5936d6510db60436173f
ulisesafcdev <ulisesafc2020@gmail.com>
2022-02-19 13:31:22 -0600

    Logica de negocio, objeto xmlhttpRequest para el consumo de API Falsa y generar contenido dinamico

---
+++ user.js
@@ -0,0 +1,38 @@
+const xhr = new XMLHttpRequest(); // intanciamos ajax
+const $container = document.querySelector('.container');
+const $template = document.getElementById('datosList').content;
+const $fragment = document.createDocumentFragment();
+
+xhr.addEventListener('readystatechange', (e) => {
+
+    if (xhr.readyState !== 4) return;
+
+    if (xhr.status >= 200 && xhr.status < 300) {
+
+        let datos = JSON.parse(xhr.responseText);
+        // console.log(datos);
+
+        datos.forEach((el) => {
+
+            $template.querySelector('h3').textContent = el.id;
+            $template.querySelector('#name').textContent = el.name;
+            $template.querySelector('#user').textContent = el.username;
+            $template.querySelector('#email').textContent = el.email;
+            $template.querySelector('#phone').textContent = el.phone;
+            $template.querySelector('#website').textContent = el.website;
+
+            let $clone = document.importNode($template, true);
+
+            $fragment.appendChild($clone);
+            
+        })
+
+        $container.appendChild($fragment);
+
+    }
+
+});
+
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+
+xhr.send();
